36b154c1fb45a2647f0747c53055617cd1bd79d0
Sandeep Kumar Murmu <90225941+sandeepKumarMurmu@users.noreply.github.com>
2024-10-21 08:38:54 +0000

    INITIATE : new code is created

---
+++ server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+// Import required modules
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse JSON request body
+app.use(express.json());
+
+// Middleware to log request details
+app.use((req, res, next) => {
+    console.log(`Request Body: ${JSON.stringify(req.body)}`);
+    console.log(`Endpoint: ${req.originalUrl}`);
+    console.log(`IP Address: ${req.ip}`);
+    console.log(`Host: ${req.hostname}`);
+    next(); // Call the next middleware or route handler
+});
+
+// Dummy GET route
+app.get('/dummy', (req, res) => {
+    res.send('GET request to /dummy');
+});
+
+// Dummy POST route
+app.post('/dummy', (req, res) => {
+    res.send('POST request to /dummy');
+});
+
+// Dummy PUT route
+app.put('/dummy', (req, res) => {
+    res.send('PUT request to /dummy');
+});
+
+// Dummy DELETE route
+app.delete('/dummy', (req, res) => {
+    res.send('DELETE request to /dummy');
+});
+
+// Dummy PATCH route
+app.patch('/dummy', (req, res) => {
+    res.send('PATCH request to /dummy');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
