4263fda7daf519b385599807e6b38e171ccdd8af
Sandeep Kumar Murmu <90225941+sandeepKumarMurmu@users.noreply.github.com>
2024-12-02 18:12:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 
 // Dummy GET route
 app.get('/dummy', (req, res) => {
-   return res.send('GET request to /dummy123459-test-again');
+   return res.send('GET request to /dummy123459-test-again-1');
 });
 
 // Dummy POST route
